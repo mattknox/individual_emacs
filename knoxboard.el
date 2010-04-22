e0e9c1b8f846036bf08adb4db95c2e4228bfd2e1
matt knox <matthewknox@gmail.com>
2010-04-22 13:23:50 -0700

    made the knoxboard theme awesome.

--- knoxboard.el
+++ knoxboard.el
@@ -1,39 +1,43 @@
-;; Knoxboard Colour Theme for Emacs.
+;; Twilight Colour Theme for Emacs.
 ;;
-;; Defines a colour scheme resembling that of the original TextMate Knoxboard colour theme.
+;; Defines a colour scheme resembling that of the original TextMate Twilight colour theme.
 ;; To use add the following to your .emacs file (requires the color-theme package):
 ;;
 ;; (require 'color-theme)
 ;; (color-theme-initialize)
-;; (load-file "~/.emacs.d/themes/color-theme-knoxboard.el")
+;; (load-file "~/.emacs.d/twilight-emacs/color-theme-twilight.el")
 ;;
-;; And then (color-theme-knoxboard) to activate it.
+;; And then (color-theme-twilight) to activate it.
 ;;
-;; MIT License Copyright (c) 2008 JD Huntington <jdhuntington at gmail dot com>
-;; Credits due to the excellent TextMate Knoxboard theme
+;; Several areas still require improvement such as recognition of code that ruby-mode doesn't
+;; yet pick up (eg. parent classes), Rails/Merb keywords, or non Ruby code related areas
+;; (eg. dired, HTML, etc). Please feel free to customize further and send in any improvements,
+;; patches most welcome.
 ;;
-;; All patches welcome
+;; MIT License Copyright (c) 2008 Marcus Crafter <crafterm@redartisan.com>
+;; Credits due to the excellent TextMate Twilight theme
 
 (require 'color-theme)
 
 ;;;###autoload
 (defun color-theme-knoxboard ()
-  "Color theme by JD Huntington, based off the TextMate Knoxboard theme, created 2008-11-27"
+  "Color theme by Marcus Crafter, based off the TextMate Twilight theme, created 2008-04-18"
   (interactive)
   (color-theme-install
-   '(color-theme-knoxboard
+   '(color-theme-twilight
      ((background-color . "black")
       (background-mode . dark)
       (border-color . "black")
       (cursor-color . "#A7A7A7")
       (foreground-color . "#F8F8F8")
       (mouse-color . "sienna1"))
-     (default ((t (:background "black" :foreground "#F8F8F8"))))
+     (default ((t (:background "black" :foreground "white"))))
      (blue ((t (:foreground "blue"))))
      (bold ((t (:bold t))))
      (bold-italic ((t (:bold t))))
      (border-glyph ((t (nil))))
-     (buffers-tab ((t (:background "black" :foreground "#F8F8F8"))))
+     (buffers-tab ((t (:background "black" :foreground "white"))))
+
      (font-lock-builtin-face ((t (:foreground "#94bff3"))))
      (font-lock-comment-face ((t (:italic t :foreground "#AEAEAE"))))
      (font-lock-constant-face ((t (:foreground "#D8FA3C"))))
@@ -51,8 +55,8 @@
      (font-lock-variable-name-face ((t (:foreground "#FF6400"))))
      (font-lock-warning-face ((t (:bold t :foreground "Pink"))))
      (gui-element ((t (:background "#D4D0C8" :foreground "black"))))
-     (region ((t (:background "#253B76"))))
-     (mode-line ((t (:background "grey75" :foreground "black"))))
+     (region ((t (:background "#444444"))))
+     (mode-line ((t (:background "#303030" :foreground "black"))))
      (highlight ((t (:background "#222222"))))
      (highline-face ((t (:background "SeaGreen"))))
      (italic ((t (nil))))
@@ -61,3 +65,4 @@
      (toolbar ((t (nil))))
      (underline ((nil (:underline nil))))
      (zmacs-region ((t (:background "snow" :foreground "ble")))))))
+
