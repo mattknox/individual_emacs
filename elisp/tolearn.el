f74b3b2cbcfcbd3642f82e923c151f5674372fe6
matt knox <matthewknox@gmail.com>
2011-03-06 18:43:13 -0800

    added a file containing stuff I wanted to learn

---
+++ elisp/tolearn.el
@@ -0,0 +1,40 @@
+;; "Emacs outshines all other editing software in approximately the
+;; same way that the noonday sun does the stars. It is not just bigger
+;; and brighter; it simply makes everything else vanish."
+;; -Neal Stephenson, "In the Beginning was the Command Line"
+
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;    Nifty things to remember and hopefully use
+
+;; ; M-z zap to char
+;; ; C-u C-SPC jump to previous edit
+;; ; M-/ autocomplete word
+;; ; M-! insert output of shell command
+;; ; M-| replace region with shell output
+;; ; M-x thumbs
+;; ; C-r k Rectangle kill
+
+;; ; C-x h select all
+;; ; C-M-\ indent
+
+;; ; Macros
+;; ; C-m C-r to begin
+;; ; name it, and do stuff
+;; ; C-s to save
+
+;; ; temp macros
+;; ; C-m C-m to start recording
+;; ; C-m C-s to stop
+;; ; C-m C-p to play
+
+;; ; M-C-p, M-C-n back and forward blocks
+;; ; C-c C-s irb when in ruby-mode
+
+;; ; C-x n n narrow visibility of buffer to selection
+;; ; C-x n w widen to full buffer
+
+;; ; Dired
+;; ; mark with 'm', press 'Q' for multi-file find/replace
+;; ; C-j launch dired when ido-mode is enabled
+
