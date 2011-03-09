e3059777fd5540b9a719ca732b813d4a79c4491b
matt knox <matthewknox@gmail.com>
2011-03-09 09:04:18 -0800

    added a registers file so I can get at my commonly-used files.

---
+++ elisp/registers.el
@@ -0,0 +1,15 @@
+;;; starter-kit-registers.el --- Set up registers
+;;
+;; Part of the Emacs Starter Kit
+
+;; Registers allow you to jump to a file or other location
+;; quickly. Use C-x r j followed by the letter of the register (i for
+;; init.el, r for this file) to jump to it.
+
+;; You should add registers here for the files you edit most often.
+
+(dolist (r `((?i (file . ,(concat user-specific-dir "/init.el")))))
+  (set-register (car r) (cadr r)))
+
+(provide 'starter-kit-registers)
+;;; starter-kit-registers.el ends here
