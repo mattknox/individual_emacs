644ac797cb2ee06a6a34da76fad4d12a091cb890
matt knox <matthewknox@gmail.com>
2011-03-16 17:48:31 -0700

    clobbered cargo-culted text.

--- elisp/registers.el
+++ elisp/registers.el
@@ -1,7 +1,3 @@
-;;; starter-kit-registers.el --- Set up registers
-;;
-;; Part of the Emacs Starter Kit
-
 ;; Registers allow you to jump to a file or other location
 ;; quickly. Use C-x r j followed by the letter of the register (i for
 ;; init.el, r for this file) to jump to it.
@@ -13,6 +9,3 @@
              (?p (file . "~/promptbird/src/main/scala/com/twitter/promptbird/Main.scala"))
              (?g (file . "~/h/goaloc/README"))))
   (set-register (car r) (cadr r)))
-
-(provide 'starter-kit-registers)
-;;; starter-kit-registers.el ends here
