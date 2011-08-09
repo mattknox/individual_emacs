097a9febf3e0601289abaad5ae7dca701eee465e
matt knox <matthewknox@gmail.com>
2011-08-09 11:47:10 -0700

    fixed typo

--- init.el
+++ init.el
@@ -289,5 +289,5 @@
 (defvar *emacs-load-time* (destructuring-bind (hi lo ms) (current-time)
                             (- (+ hi lo) (+ (first *emacs-load-start*)
                                             (second *emacs-load-start*)))))
-(message "My .emacs loaded in %ds" *emacs-load-start*)
+(message "My .emacs loaded in %ds" *emacs-load-time*)
 
